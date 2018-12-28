cd57d5ecd9e8c0c10d78e9ddc6abaf653b350869
jcapineri <46034227+jcapineri@users.noreply.github.com>
2018-12-28 11:25:33 -0200

    deploy

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET test page. */
+router.get('/test', function(req, res, next) {
+  res.render('test');
+});
+
+/* POST test page. */
+router.post('/test', function(req, res, next) {
+  var nombre = req.body.nombre;
+  var edad = req.body.edad;
+
+  res.send(`
+    <h1>Datos Ingresados:</h1>
+    <div>
+      <h1>Nombre: [` + nombre + `] Edad: [` + edad + `]</h1>
+      <button name="boton" onclick="window.location='http://localhost:3000'">Volver</button>
+    </div>
+  `)    
+});
+
 module.exports = router;
